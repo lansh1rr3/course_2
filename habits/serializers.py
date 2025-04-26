73d872d328903b4f7593319768f2d798123acce4
Student <maksfanloker@gmail.com>
2025-04-26 13:27:43 +0300

    15

--- habits/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Habit
-
-
-class HabitSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Habit
-        fields = '__all__'
-        read_only_fields = ('user',)
